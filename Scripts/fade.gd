7bd59afd0c383ca2c501cb2c6836946852acd4ed
Nathã William <nathawilliam143@gmail.com>
2026-09-12 14:26:13 -0300

    added fade scene for transitions

---
+++ Scripts/fade.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+class_name Fade
+
+@export var duration := 0.5
+
+@onready var _rect = $FadeRect
+
+func _ready() -> void:
+	_rect.modulate.a = 0.0
+
+func fade_in():
+	var tween := create_tween()
+	
+	tween.tween_property(_rect, "modulate:a", 1.0, duration)
+	await tween.finished
+
+func fade_out():
+	var tween := create_tween()
+	
+	tween.tween_property(_rect, "modulate:a", 0.0, duration)
+	await tween.finished
